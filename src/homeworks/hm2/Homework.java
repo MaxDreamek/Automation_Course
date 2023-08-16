c5528a579a0c4aa295c82c67fe3873b632383ae5
Max Styosov <avardn@gmail.com>
2023-08-16 21:23:06 +0300

    First lesson

---
+++ src/homeworks/hm2/Homework.java
@@ -0,0 +1,10 @@
+package homeworks.hm2;
+
+public class Homework {
+    public static void main(String[] args) {
+        int alex = 7;
+        int maks = 9;
+        maks++;
+        System.out.println(alex + maks);
+    }
+}
---
+++ src/lessons/lesson2/Variables.java
@@ -0,0 +1,10 @@
+package lessons.lesson2;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age  = 5;
+        int ageOfEmployee;
+        System.out.print("Hello world" + "\n");
+        System.out.println("Hello World");
+    }
+}
