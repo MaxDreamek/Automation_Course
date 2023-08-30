e626a680f2e952f92ff39f588c630d954230afc9
Max Styosov <avardn@gmail.com>
2023-08-30 22:31:08 +0300

    Lesson6, refactoring

--- src/lessons/lesson6/ArrayReverse.java
+++ src/lessons/lesson6/ArrayReverse.java
@@ -1,6 +1,5 @@
 package lessons.lesson6;
 
-import java.util.Arrays;
 
 public class ArrayReverse {
     public static void main(String[] args) {
@@ -9,15 +8,14 @@ public class ArrayReverse {
 
     public static void arrRev() {
         String[] arr = {"Каждый", "Охотник", "хочет", "знать", "где", "сидит", "фазан"};
-        int arrLen = arr.length;
 
         String temp;
         //length / 2
-        // a - i - 1
+        // arr.length - i - 1
 
-        for (int i = 0; i < arrLen / 2; i++) {
-            temp = arr[arrLen - i - 1];
-            arr[i] = arr[arr.length - i - 1] = arr[i];
+        for (int i = 0; i < arr.length / 2; i++) {
+            temp = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = arr[i];
             arr[i] = temp;
         }
         for (String s : arr) {
