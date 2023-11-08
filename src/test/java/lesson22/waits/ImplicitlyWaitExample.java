abb8111fd36bece8c6b94d89335d73ba4af9c6a3
Max Styosov <avardn@gmail.com>
2023-11-08 20:04:33 +0200

    Lesson22

---
+++ src/test/java/lesson22/waits/ImplicitlyWaitExample.java
@@ -0,0 +1,25 @@
+package lesson22.waits;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class ImplicitlyWaitExample {
+    private WebDriver driver;
+    @BeforeTest
+    public void initDriver(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @Test
+    public void testWithImplicitWait(){
+        driver.get("https://www.google.com.ua/");
+        driver.findElement(By.xpath("//div[@class='FPdoLc lJ9FBc']//input[@value='Мне повезёт!']")).click();
+    }
+}
