1dce737f465f8d968c2b4b0bd76002cc76b06866
Max Styosov <avardn@gmail.com>
2023-11-15 19:58:38 +0200

    Lesson24

--- src/test/java/init/WebDriverInit.java
+++ src/test/java/init/WebDriverInit.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
@@ -17,7 +19,12 @@ public class WebDriverInit {
     @BeforeTest
     public void initDriver() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(new ChromeOptions().addArguments("--start-maximized"));
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("--start-maximized");
+        chromeOptions.addArguments("--disable-automation");
+//        chromeOptions.addArguments("--headless");
+        driver = new ChromeDriver(chromeOptions);
         webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
     }
---
+++ src/test/java/lesson24/TestWithClear.java
@@ -0,0 +1,19 @@
+package lesson24;
+
+import init.WebDriverInit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestWithClear extends WebDriverInit {
+    @Test
+    public void testClear() throws InterruptedException {
+        driver.get("https://www.google.com.ua");
+        WebElement inputSearch = driver.findElement(By.id("APjFqb"));
+        inputSearch.sendKeys("Hello word");
+        Thread.sleep(5000);
+        inputSearch.clear();
+        Thread.sleep(5000);
+    }
+}
