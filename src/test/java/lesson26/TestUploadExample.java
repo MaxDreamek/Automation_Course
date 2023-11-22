42da50b78ef65dd5e14d4b7f6c1dfc3ebc5d626e
Max Styosov <avardn@gmail.com>
2023-11-22 20:45:37 +0200

    lesson26

---
+++ src/test/java/lesson26/TestUploadExample.java
@@ -0,0 +1,21 @@
+package lesson26;
+
+import init.WebDriverInit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class TestUploadExample extends WebDriverInit {
+
+    @Test
+    public void testUpload() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/upload");
+        WebElement uploadElement = driver.findElement(By.id("file-upload"));
+        WebElement uploadButton = driver.findElement(By.id("file-submit"));
+
+        uploadElement.sendKeys("D:\\Automation_Course\\somefile.txt");
+        uploadButton.click();
+        Thread.sleep(5000);
+    }
+}
