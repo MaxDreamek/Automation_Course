5c240bef4446a26f369284e3cc7d793cb71e638f
Max Styosov <avardn@gmail.com>
2023-11-27 19:22:02 +0200

    My Homework2

--- src/test/java/homeworks/homework25/Test2.java
+++ src/test/java/homeworks/homework25/Test2.java
@@ -1,6 +1,11 @@
 package homeworks.homework25;
 
-public class Test2 {
+
+import pageobject.init.WebDriverInit;
+import org.testng.annotations.Test;
+
+public class Test2 extends WebDriverInit {
+    @Test
     public void test22(){
 
     }
