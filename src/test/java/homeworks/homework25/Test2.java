9badee2e4f755012c40533e795aa2ace01f40136
Max <130498345+MaxDreamek@users.noreply.github.com>
2023-11-27 19:39:29 +0200

    My Homework2 (#26)
    My Homework2

---
+++ src/test/java/homeworks/homework25/Test2.java
@@ -0,0 +1,12 @@
+package homeworks.homework25;
+
+
+import pageobject.init.WebDriverInit;
+import org.testng.annotations.Test;
+
+public class Test2 extends WebDriverInit {
+    @Test
+    public void test22(){
+
+    }
+}
